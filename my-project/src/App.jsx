cfe03555af8b150b20d5a51fbe045d6251469177
gajanand <gajanand2005singh@gmail.com>
2025-09-23 11:30:52 +0530

    commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+     <h1 className='text-3xl text-red-500
+      font-bold'>Good morning</h1> 
+    
+    </>
+  )
+}
+
+export default App
