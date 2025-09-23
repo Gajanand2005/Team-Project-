2111dec0a40f52afade5b996d4c91ffa9c5d5084
gajanand <gajanand2005singh@gmail.com>
2025-09-23 20:56:14 +0530

    commit

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Header from './components/Header'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
 const App = () => {
   return (
     <>
+    <BrowserRouter>
      <Header /> 
+     <Routes>
+      <Route path={'/'} exact={true} element={<Home/>} />
+     </Routes>
+     </BrowserRouter>
     </>
   )
 }
--- my-project/src/components/Header/index.jsx
+++ my-project/src/components/Header/index.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Header = () => {
   return (
-    <div>
-      Header Component
-    </div>
+    <>
+      <header>
+      <div className='top-strip py-2 '>
+
+        <div className='container'>
+          <div className='flex items-center justify-between'>
+            <div className='col1 w-[50%]'>
+              <p className='text-[14px] font-[500]'>Get up to 50% off new season styles, limited time </p>
+            </div>
+
+            <div className='flex items-center justify-end col2'>
+              <ul>
+                <li className='list-none'>
+                  <Link to='/help-center'>Help Center</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      </header>
+    </>
   )
 }
 
