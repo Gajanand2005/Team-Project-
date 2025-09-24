63059e6b5b326ef0ddcf0f0776fb145c4bd97d3b
gajanand <gajanand2005singh@gmail.com>
2025-09-24 20:12:20 +0530

    commit

--- my-project/src/components/Header/index.jsx
+++ my-project/src/components/Header/index.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/logo.jpg'
 import Search from '../Search'
+
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import { FaLuggageCart } from "react-icons/fa";
+
+const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    right: -3,
+    top: 13,
+    border: `2px solid ${(theme.vars ?? theme).palette.background.paper}`,
+    padding: '0 4px',
+  },
+}));
+
 const Header = () => {
   return (
     <>
@@ -33,7 +48,22 @@ const Header = () => {
             <div className='col2 w-[45%] '>
               <Search/>
             </div>
-            <div className='col3 w-[30%]'></div>
+            <div className='col3 w-[30%] flex items-center pl-7'>
+                <ul className='flex items-center gap-3'>
+                  <li className='list-none'>
+                    <Link to='/login' className='link transition text-[15px] font-[500]'>Login</Link> | &nbsp;
+                    <Link to='/register' className='link transition text-[15px] font-[500]'>Register</Link>
+
+                  </li>
+                  <li>
+                      <IconButton aria-label="cart">
+                     <StyledBadge badgeContent={4} color="secondary">
+                   <FaLuggageCart />
+                 </StyledBadge>
+                </IconButton>
+                  </li>
+                </ul>
+            </div>
           </div>
         </div>
       </header>
--- my-project/src/components/Search/index.jsx
+++ my-project/src/components/Search/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import '../Search/style.css'
+import Button from '@mui/material/Button';
+import { BsSearchHeart } from "react-icons/bs";
 
 const Search = () => {
   return (
     <div>
       <div className='searchbox w-[100%] h-[50px] bg-[#e5e5e5] rounded-[5px] relative p-2'>
         <input type="text" placeholder='Search for products.....' className='w-full h-[35px] focus:outline-none bg-inherit p-2 text-[15px]' />
+          <Button className='!absolute top-[8px] right-[5px] z-50 !w-[37px] !min-w-[37px] h-[35px] !rounded-full !text-black'><BsSearchHeart className='text-[#545050] text-[22px]' /></Button>
       </div>
     </div>
   )
