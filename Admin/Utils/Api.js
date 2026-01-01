74fef9ccf17a9aec0ad327e77d75838ca325ebfa
gajanand <gajanand2005singh@gmail.com>
2026-01-01 18:47:09 +0530

    all things are done

--- Admin/Utils/Api.js
+++ Admin/Utils/Api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const apiUrl = 'https://team-project-production.up.railway.app' || 'http://localhost:8000';
+const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+
 
 
 /* ================= TOKENS ================= */
