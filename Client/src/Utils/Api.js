86ec5967182d6bf68b4d86313da97c9a369576c0
gajanand <gajanand2005singh@gmail.com>
2026-01-01 18:01:46 +0530

    all done

--- Client/src/Utils/Api.js
+++ Client/src/Utils/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const apiUrl = 'https:team-project-production.up.railway.app' || 'http://localhost:8000';
 
 
 const getAccessToken = () => localStorage.getItem('accessToken');
